65b24d1b76a170c0db23bc36172ace2576bdd57f
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2025-08-23 11:32:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    'jjsbrbd': 'https://www.google.com',
+    'QR1': 'https://imgur.com/a/got-you-45Qpn5g',
     'another': 'https://github.com/another-example',
     'coolsite': 'https://www.example.com',
     'test1': 'https://www.bing.com',
