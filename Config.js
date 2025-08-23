543dd9b87fd86abc169a231f49046e214da3468e
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2025-08-23 11:18:06 +0200

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,7 @@
+const config = {
+    'jjsbrbd': 'https://www.google.com',
+    'another': 'https://github.com/another-example',
+    'coolsite': 'https://www.example.com',
+    'test1': 'https://www.bing.com',
+    'test2': 'https://www.yahoo.com'
+};
