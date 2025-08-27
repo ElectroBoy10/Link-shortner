72280fcda1214ab7a246be31894083a30329a9c9
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2025-08-27 05:36:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    'QR1': 'https://imgur.com/a/got-you-45Qpn5g',
+    'BUa8b2': 'https://imgur.com/a/got-you-45Qpn5g',
     'another': 'https://github.com/another-example',
     'coolsite': 'https://www.example.com',
     'test1': 'https://www.bing.com',
