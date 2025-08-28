e61c96929f7e340709a3ff45f95eea2e697d4647
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2025-08-28 06:13:40 +0200

    Update config.js
    New lock addon

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-    'BUa8b2': 'https://imgur.com/a/got-you-45Qpn5g',
-    'another': 'https://github.com/another-example',
-    'coolsite': 'https://www.example.com',
-    'test1': 'https://www.bing.com',
-    'test2': 'https://www.yahoo.com'
+  'BUa8b2': { url: 'https://imgur.com/a/got-you-45Qpn5g', locked: 0 },
+  'another': { url: 'https://github.com/another-example', locked: 0 },
+  'coolsite': { url: 'https://www.example.com', locked: 1, password: 'secret123' },
+  'test1': { url: 'https://www.bing.com', locked: 0 },
+  'test2': { url: 'https://www.yahoo.com', locked: 0 }
 };
