3436afc367ad6f87b2a824e54524ed8c3088c216
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2025-11-21 09:37:23 +0200

    Update config.js with new site entries

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
   'BUa8b2': { url: 'https://imgur.com/a/got-you-45Qpn5g', locked: 0 },
   'another': { url: 'https://github.com/another-example', locked: 0 },
-  'coolsite': { url: 'https://www.example.com', locked: 1, password: 'secret123' },
-  'test1': { url: 'https://www.bing.com', locked: 0 },
-  'test2': { url: 'https://www.yahoo.com', locked: 0 }
+  'BingTest': { url: 'https://www.bing.com', locked: 1, password: 'secret123' },
+  'Voltsim': { url: 'https://discord.gg/AC8MhtVfF6', locked: 0 }
 };
