79fc3d48fe939b80325c4f78d6b8d90ef361331a
MrBros <120547605+ElectroBoy10@users.noreply.github.com>
2026-01-20 18:41:38 +0200

    Delete config.js
    Not needed anymore

--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  'BUa8b2': { url: 'https://imgur.com/a/got-you-45Qpn5g', locked: 0 },
-  'another': { url: 'https://github.com/another-example', locked: 0 },
-  'BingTest': { url: 'https://www.bing.com', locked: 1, password: 'secret123' },
-  'Voltsim': { url: 'https://discord.gg/AC8MhtVfF6', locked: 0 }
-};
